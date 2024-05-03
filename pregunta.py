080e95a8a04507718f9f80b9cb347a54c6f29757
Nivatru <nvalenciat@unal.edu.co>
2024-05-02 19:28:24 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,23 @@ espacio entre palabra y palabra.
 
 
 """
+
 import pandas as pd
 
 
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
-
+    df = pd.read_fwf("clusters_report.txt", widths=[9, 16, 16, 77])
+    df.columns = [col.strip().replace(" ", "_").lower() for col in df.columns]
+    df.rename(columns = {'cantidad_de':'cantidad_de_palabras_clave', 'porcentaje_de':'porcentaje_de_palabras_clave'}, inplace = True)
+    df.drop([0,1], inplace=True)
+    df.cluster.ffill(inplace=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].groupby(df['cluster']).transform(lambda x: ' '.join(x))
+    df.cantidad_de_palabras_clave.ffill(inplace=True)
+    df.porcentaje_de_palabras_clave.ffill(inplace=True)
+    df.drop_duplicates(inplace=True)
+    df.reset_index(inplace=True, drop=True)
+    df.cluster = df.cluster.astype(int)
+    df.cantidad_de_palabras_clave = df.cantidad_de_palabras_clave.astype(int)
+    df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.str.replace('%','').str.replace(',','.').astype(float)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace('    ', ' ').str.replace('   ', ' ').str.replace('  ', ' ').str.replace(',,', ',').str.replace('.', '')
     return df
